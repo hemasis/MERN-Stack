6fa5e826bfe68b26ed9abe0d9f3fe4c5b5e3aa2e
Hemasis Padhi <hemasis1910@gmail.com>
2025-06-20 02:05:31 +0530

    Day 1

---
+++ Day 1/index.js
@@ -0,0 +1,78 @@
+console.log('Hello Developers')
+console.log('name: Hemasis')
+console.log('LPU')
+
+let a = 10;
+a = 20;
+console.log(a);
+
+let x = 10;
+let y = 20;
+let res = x + y;
+console.log(res);
+
+// ************************************************************************
+
+var name = "Hemasis";    //old way
+let age = 25;            //new way
+const county = "India";  //
+
+
+/* ************** DATA TYPES ************** */
+// 1) Primitive data types:
+//  String
+ const country = "India";
+console.log(typeof country); 
+
+//  Number
+let Age = "24";
+console.log(typeof Age);
+
+//  Boolean
+let isLoggedIn = true;
+console.log(typeof isLoggedIn);
+
+//  Undefined
+let score;
+console.log(typeof score);
+
+//  Null
+let Score = null;
+console.log(typeof Score);
+
+// Symbol
+let sym = Symbol('unique');
+console.log(typeof sym);
+
+// BigInt
+let bigNumber = BigInt(1234567890123456789012345678901234567890);
+console.log(typeof bigNumber);
+
+
+/* ************** OPERATORS ************** */
+// 1) Arithmetic Operators (+, -, *, /)
+let p = 10;
+let q = 20;
+console.log(p+q);
+console.log(p-q);
+console.log(p*q);
+console.log(p/q);
+
+
+// 2) Assignment Operators (=, +=, -=, *=, /=)
+let s = 30;
+// s += 10;
+// s -= 10;
+// s *= 2;
+s /= 5;
+console.log(s);
+
+// 3) Comparison Operators (==, ===, !=, !==, >, <, >=, <=)
+let roll_no = 30;
+let num = "6";
+console.log(roll_no != num);  // answer will be true
+console.log(roll_no == num);  // answer will be false
+console.log(roll_no === num);
+
+// 4) Logical Operators
+// Others
