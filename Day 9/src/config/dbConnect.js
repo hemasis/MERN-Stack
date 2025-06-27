a5ca9aa4f2a5084f01c7feba88ae3a50deaa5a4b
Hemasis Padhi <hemasis1910@gmail.com>
2025-06-28 01:28:12 +0530

    Modified

--- Day 9/src/config/dbConnect.js
+++ Day 9/src/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("...")
+        await mongoose.connect("mongodb+srv://hemasisp:1234@cluster0.ramls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("✅ MongoDB connected");
     } catch (error) {
         console.log("❌ MongoDB connection Failed: ",error.message);
