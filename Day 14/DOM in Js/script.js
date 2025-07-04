059c2ace17e895d49d2cb61e8dda564f7e8d3e09
Hemasis Padhi <hemasis1910@gmail.com>
2025-07-04 10:42:01 +0530

    DOM in Js

---
+++ Day 14/DOM in Js/script.js
@@ -0,0 +1,11 @@
+const data
+
+const main = document.querySelector("h1")
+
+
+main.addEventListener("click", abc)
+function abc() {
+    main.style.color = "red";
+    main.innerHTML = " ha ha ha ha";
+    
+}
