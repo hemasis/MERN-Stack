aa32d70863b77763789b19bb2a7f9bab917b97ab
Hemasis Padhi <hemasis1910@gmail.com>
2025-07-05 14:07:35 +0530

    React basics

--- Day 15/viteproject/src/App.jsx
+++ Day 15/viteproject/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Class from './Class.jsx'
 
+
+let name = 'React'
 function App() {
   // const [count, setCount] = useState(0)
 
@@ -8,6 +10,7 @@ function App() {
     <>
       <h2>Hellllloooo!</h2>
       <Class />
+      {name}
     </>
   )
 }
