9abbcc54c4282348f98245adb9f91f7fc10d591f
Hemasis Padhi <hemasis1910@gmail.com>
2025-07-07 09:17:34 +0530

    react basics

---
+++ Day 15/02-viteproject/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Class from './Class.jsx'
+
+
+let name = 'React'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Hellllloooo!</h2>
+      <Class />
+      {name}
+    </>
+  )
+}
+
+export default App
